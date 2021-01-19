29350b922e166ebf8bb933d681419687c7289fc4
Pete Matsyburka <pete.matsy@gmail.com>
2021-01-19 14:29:33 +0200

    remove redundant loader ignore

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,6 @@ module SiteInspector
     config.autoload_paths << Rails.root.join('lib')
     config.eager_load_paths << Rails.root.join('lib')
 
-    Rails.autoloaders.main.ignore(Rails.root.join('lib/name_dataset'))
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
