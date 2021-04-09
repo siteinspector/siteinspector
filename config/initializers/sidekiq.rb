7416085276198b68ef5450f8f3584bc9620dc6c6
Pete Matsyburka <pete.matsy@gmail.com>
2021-04-09 17:29:45 +0300

    fix heroku build

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,6 +8,3 @@ require 'sidekiq/dynamic_queues_web'
 Sidekiq::DynamicQueues.setup! if Sidekiq::DynamicQueues.enabled?
 
 Sidekiq::Web.register(Sidekiq::DynamicQueuesWeb)
-
-Sidekiq::Web.set(:session_secret, Rails.application.secrets[:secret_key_base])
-Sidekiq::Web.set(:sessions, Rails.application.config.session_options)
