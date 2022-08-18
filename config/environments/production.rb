a3b79b368fbfb6bc67b118c2b97d42fec3555a1d
Pete Matsyburka <pete.matsy@gmail.com>
2022-08-19 00:20:18 +0300

    fix rubocop

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV['RAILS_LOG_TO_STDOUT'] != "false"
+  if ENV['RAILS_LOG_TO_STDOUT'] != 'false'
     logger           = ActiveSupport::Logger.new($stdout)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
